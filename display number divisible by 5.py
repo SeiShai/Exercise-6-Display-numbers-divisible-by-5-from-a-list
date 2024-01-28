97c6e55f3bdf3192ca1a2f93b65b19ae723fa609
Shai_Base <shaibase1328@gmail.com>
2024-01-28 12:33:35 +0800

    created the list

---
+++ display number divisible by 5.py
@@ -0,0 +1,9 @@
+# Iterate the given list of numbers and
+# print only those numbers which are divisible by 5
+
+# create the list
+number_list = [10, 20, 33, 46, 55]
+
+print('The given numbers are:', number_list)
+print('the numbers divisible by 5 are:')
+
