f1f32c9dd1678dc5c975d4ea050d33a9976af337
Shai_Base <shaibase1328@gmail.com>
2024-01-28 12:34:14 +0800

    added iterations

--- display number divisible by 5.py
+++ display number divisible by 5.py
@@ -7,3 +7,7 @@ number_list = [10, 20, 33, 46, 55]
 print('The given numbers are:', number_list)
 print('the numbers divisible by 5 are:')
 
+# make iteration
+for numbers in number_list:
+    if numbers % 5 == 0:
+        print(numbers)
